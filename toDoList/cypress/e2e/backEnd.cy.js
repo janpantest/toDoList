2dd628a09c1851cd88ad78de426c89a5517a5f30
janpantest <krchovaak@gmail.com>
2022-10-24 19:34:52 +0200

    backend test added

--- toDoList/cypress/e2e/backEnd.cy.js
+++ toDoList/cypress/e2e/backEnd.cy.js
@@ -1,45 +1,49 @@
 
 describe('api test', () => {
 
-  
+  const taskName = (Math.random() + 1).toString(36).substring(7);
 
-  it('Login incorrect', function () {
+  it('back end', function () {
     cy.request({
       method: 'POST',
       url: 'https://localhost:5001/api/ToDoItems',
       body: {
-        'text': 'newTask'
+        'text': taskName
       }
-    }).then((res) =>{
-      // expect(res.status).to.eq(201);
-      // expect(res.body).has.property('text', 'newTask')
-      // cy.log(JSON.stringify(res))
-      // cy.log(res.body.id)
-      // console.log(JSON.stringify(res.body.id))
-      const taskId = res.body.id
+    }).then((resPost) =>{
+      const taskId = resPost.body.id
       return taskId
-      // cy.log(JSON.stringify(res))
     })
-    .then((taskId) => {
-        cy.request({
-          method: 'GET',
-          url: 'https://localhost:5001/api/ToDoItems/'+taskId
-        }).then((resp) =>{
-          expect(resp.status).to.eq(200)
-          expect(resp.body).to.have.property('id', taskId)
-        })
-    })
-    // .then((taskId) =>{
-    //   cy.request({
-    //     method: 'DELETE',
-    //     url: 'https://localhost:5001/api/ToDoItems/'+taskId
-    //   }).then((resp) =>{
-    //     expect(resp.status).to.eq(200)
-    //   })
-    // })
+      .then((taskId) => {
+          cy.request({
+            method: 'GET',
+            url: 'https://localhost:5001/api/ToDoItems/'+taskId
+          }).then((resGet) =>{
+            expect(resGet.status).to.eq(200)
+            expect(resGet.body).to.have.property('id', taskId)
+            }).then((res) => {
+                cy.request({
+                  method: 'DELETE',
+                  url: 'https://localhost:5001/api/ToDoItems/'+taskId
+                }).then((resDel) => {
+                  expect(resDel.status).to.eq(204)
+                  }).then((res) => {
+                    cy.request({
+                      method: 'GET',
+                      url: 'https://localhost:5001/api/ToDoItems/'
+                    }).then((resGet1) =>{
+                      expect(resGet1.status).to.eq(200)
+                      cy.log(JSON.stringify(resGet1))
+                      // expect(resGet1.body).to.have.property('id', )
+                      expect(resGet1.body).not.to.have.property('id',   )
+                      })
+
+
+                  })
+              })
+          })
+
   })
-
-
 
 })
 
